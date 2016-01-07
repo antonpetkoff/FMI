53922e8fffe62c4df42130953c95945b04a92edc
tonynho <tonynho010@gmail.com>
2016-01-07 22:10:58 +0200

    NM and haskell

--- Functional/Haskell/intro.hs
+++ Functional/Haskell/intro.hs
@@ -1,3 +1,5 @@
+import Prelude hiding (elem, (++), (!!))
+
 quickSort [] = []
 quickSort (x:xs) = quickSort less ++ [x] ++ quickSort more
   where less = filter (<=x) xs
@@ -18,7 +20,37 @@ zad1 ll x i = elem x (ll !! (quot i 3))
 
 fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
 
-main :: IO Int
+-- l1 ++ l2 = if null l1 then l2 else head l1 : tail l1 ++ l2
+
+[] ++ l2 = l2
+(x:xs) ++ l2 = x:xs ++ l2
+
+-- l !! n
+--  | n == 0    = head l
+--  | otherwise = tail l !! (n - 1)
+
+(!!) :: [a] -> Int -> a
+(x:_) !! 0 = x
+(_:xs) !! n = xs !! (n - 1)
+
+
+len [] = 0
+len l = len (tail l) + 1
+
+-- elem x l
+--  | null l       = False
+--  | head l == x  = True
+--  | otherwise    = elem x (tail l)
+
+elem :: Eq a => a -> [a] -> Bool
+elem _ [] = False
+elem x (y:ys) = x == y || elem x ys
+
+type Int3 = [(Int, Int, Int)]
+pythagoreanTriple :: Int -> Int3
+pythagoreanTriple n = 
+    [(x,y,z) | x <- [1..n], y <- [1..x-1], z <- [max x y..n], x^2 + y^2 == z^2]
+
 main = do
         -- print (factorial 5)
         -- print (l !! 0)
@@ -26,5 +58,9 @@ main = do
         -- print ([x * 2 | x <- [0..10000], x `mod` 85 == 23]) -- remember the ``
         -- print (myFilter odd [1..10])
         -- print (zad1 mat 15 15)
-        print (take 10 fibs)
-        return 10
+        -- print (take 10 fibs)
+        -- print ([1] ++ [2, 3])
+        -- print ([1,2,3] !! 2)
+        -- print (elem 4 [1..5])
+        -- print (len [1..5])
+        print (pythagoreanTriple 20)
