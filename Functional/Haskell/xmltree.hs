15e82091e406fb9f1a11aa74e767acaa871ea3c1
tonynho <tonynho010@gmail.com>
2016-01-21 23:31:05 +0200

    haskell project

--- Functional/Haskell/xmltree.hs
+++ Functional/Haskell/xmltree.hs
@@ -1,29 +1,59 @@
 module Main where
 
+import Text.Regex.Posix
+
 data Attribute = Attribute {name :: String, value :: String} deriving Show
-data Node = Compound {tag :: String, children :: [Node], attributes :: [Attribute]}
+data Tree = Tree {tag :: String, children :: [Tree], attributes :: [Attribute]}
           | Leaf {tag :: String, text :: String, attributes :: [Attribute]} deriving Show
+-- if Leaf has no text, it is a self-closing tag
+-- assert :: Tree always has at least 1 child
 
 attr1 = Attribute "src" "madonna.jpg"
 attr2 = Attribute "encoding" "UTF-8"
 
-leaf1 = Leaf "img" "myImgText" [attr1, attr2]
+leaf1 = Leaf "img" "" [attr1, attr2]
 leaf2 = Leaf "p" "myParagraph" [attr2]
+leaf3 = Leaf "p" "myParagraph2" [attr2]
 
-compound1 = Compound "div" [leaf1, leaf2] []
+tree1 = Tree "div" [leaf1, leaf2, leaf3] []
 
 showAttributes :: [Attribute] -> String
 showAttributes [] = ""
 showAttributes attributes = concat [" " ++ name attr ++ "=\"" ++ value attr ++ "\"" | attr <- attributes]
 
-showLeaf :: Node -> String
-showLeaf (Leaf tag text attributes) = concat ["<", tag, showAttributes attributes, ">", text, "</", tag, ">"]
+showLeaf :: Tree -> String
+showLeaf (Leaf tag text attributes)
+    | text == ""    = concat ["<", tag, showAttributes attributes, "/>"]
+    | otherwise     = concat ["<", tag, showAttributes attributes, ">", text, "</", tag, ">"]
+
+tabs :: Int -> String
+tabs n = concat $ replicate n "\t"
+
+showTree :: Int -> Tree -> String
+showTree level leaf@(Leaf _ _ _) = concat [tabs level, showLeaf leaf]
+showTree level (Tree tag children attributes) = concat [openingTag, content, closingTag]
+    where
+        openingTag = concat [tabs level, "<", tag, showAttributes attributes, ">\n"]
+        content = unlines $ map (showTree (level + 1)) children
+        closingTag = concat ["</", tag, ">"]
+
+
+-- parseAttribute :: String -> Attribute
+
+
+-- parses a leaf node which can be self-closing too
+-- parseLeaf :: String -> Tree
+-- parseLeaf line = (Tree tag, text, attributes)
+--     where
+--         matched = line =~ "<([a-z]+)(.*)/>" :: [[String]]
+--         tag = matched !! 0 !! 1
+--         attributes = parseAttributes $ words matched !! 0 !! 2
+
+-- match
+
+myTag = "<img src=\"madonna.jpg\" encoding=\"UTF-8\"/>"
 
 main = do
-    print [attr1, attr2]
-    print leaf1
-    print (tag leaf1)
-    print (tag compound1)
-    print (attributes leaf1)
-    print (showAttributes [attr1, attr2])
-    print (showLeaf leaf1)
+    writeFile "output.xml" (showTree 0 tree1)
+    print (myTag =~ "<([a-z]+)(.*)/>" :: [[String]]) 
+    print (getAllTextMatches $ myTag =~ "\\w+=\"[\\w\\s\\.\\-]+\"" :: [String])
