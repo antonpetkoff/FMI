406ee57030545911a7457f98f9ea80e27bb60453
tonynho <tonynho010@gmail.com>
2016-03-21 12:26:57 +0200

    OS hw1

--- OS/hw1/groupbyext.sh
+++ OS/hw1/groupbyext.sh
@@ -1,8 +1,8 @@
-# source=$1, target=$2
+# $1=source, $2=target
+pattern='\.[a-z0-9]+$'
 
-dirs=$(find $1 -type f -regextype posix-extended -regex '.*\.[[:alnum:]]+' -print)
-a
-for file in $dirs; do
-    echo $file
+find $1 -type f | grep -Ei $pattern | while read f
+do
+    extension=$(echo $f | grep -Eio $pattern | cut -c2-)
+    mkdir -p "$2$extension" && cp "$f" "$2$extension"
 done
-
