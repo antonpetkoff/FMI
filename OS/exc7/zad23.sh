bc7159d49d1bb7d92d1ed12b58f7de07b2d9fc07
tonynho <tonynho010@gmail.com>
2016-04-11 14:19:49 +0300

    exc 3

--- OS/exc7/zad23.sh
+++ OS/exc7/zad23.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 
+# $1 = dir, $2 = fileSize
+
+if [ -d $1 ]
+then find $1 -maxdepth 1 -type f -exec du -b {} \; | while read line
+    do
+        if [ $(echo $line | awk '{print $1}') -gt "$2" ]
+        then echo $line | awk '{print $2}'
+        fi
+    done
+fi
