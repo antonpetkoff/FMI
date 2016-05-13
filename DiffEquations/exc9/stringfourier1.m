43d50283a6c22766bd4202d9193ee1e156bbeccc
tonynho <anton.petkoff@gmail.com>
2016-05-13 13:08:26 +0300

    diff eq

--- DiffEquations/exc9/stringfourier1.m
+++ DiffEquations/exc9/stringfourier1.m
@@ -4,21 +4,31 @@ clf;
 a = 0.5;
 L = pi * sqrt(2);
 tmax = 30;
-t = 0:tmax/100:tmax;
+t = 0:tmax/1000:tmax;
 x = 0:L/100:L;
 
     function y = phi(x)
-        for i=1:length(x)
-            if 1 < x(i) && x(i) < 2
-                y(i) = 10 * exp(4/((2*x(i)-3)^2 -1));
-            else
-                y(i) = 0;
-            end
-        end
+%         % a)
+%         for i=1:length(x)
+%             if 1 < x(i) && x(i) < 2
+%                 y(i) = 10 * exp(4/((2*x(i)-3)^2 -1));
+%             else
+%                 y(i) = 0;
+%             end
+%         end
+
+%         % б)
+%         y = sin(3*x*sqrt(2));
+        
+        % в)
+        y = 0
     end
 
     function y = psi(x)
-        y = 0;
+%           % a) и б)
+%         y = 0 * x;
+        % в)
+        y = 1/4 * sin(5*x*sqrt(2))
     end
 
     function y = fourier(x, t)
@@ -33,9 +43,20 @@ x = 0:L/100:L;
     end
 
 for n=1:length(t)
-    plot(x, fourier(x, t(n)));
-    axis([0, L, -0.5, 0.5]);
-    M(k)=getframe;
+   plot(x, fourier(x, t(n)));
+   axis([0, L, -0.5, 0.5]);
+   M(n)=getframe;
 end
+movie(M,2)
+
+% % subplots
+% tt = [0, 15, 20];
+% for m = 1:3
+%     subplot(3, 1, m);
+%     plot(x, fourier(x, tt(m)));
+% 
+%     axis([0, L, -0.5, 0.5]);
+% end
+
 
 end
