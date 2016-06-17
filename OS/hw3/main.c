8c680323a7ea2e23215fc008e49198383dd13c95
tonynho <anton.petkoff@gmail.com>
2016-06-17 19:34:33 +0300

    OS hw3 better solution

--- OS/hw3/main.c
+++ OS/hw3/main.c
@@ -5,14 +5,17 @@
 #include <err.h>
 #include <stdio.h>
 #include <string.h>
+#define __STDC_FORMAT_MACROS
+#include <inttypes.h>
 
 int main(int argc, char** argv) {
     int fd;
-    unsigned char c;
+    ssize_t read_size;
+    uint8_t buffer[1<<12];
     const int SIZE = 1<<8;
-    unsigned long long chars[SIZE];
-    memset(chars, 0, SIZE * sizeof(unsigned long long));
-
+    uint64_t bytes[SIZE];
+    memset(bytes, 0, SIZE * sizeof(uint64_t));
+    
     if (argc != 2) {
         err(1, "Exactly one argument must be passed!");
     }
@@ -21,16 +24,16 @@ int main(int argc, char** argv) {
         err(1, "Could not open file for reading");
     }
 
-    while (read(fd, &c, 1) > 0) {
-        chars[(int) c]++;
+    while ((read_size = read(fd, &buffer, sizeof(buffer))) > 0) {
+        for (int i = 0; i < read_size; ++i) {
+            bytes[(uint8_t) buffer[i]]++;
+        }
     }
     close(fd);
 
     printf("%s", "BYTE\tCOUNT\n");
     for (int i = 0; i < SIZE; ++i) {
-        // the byte with decimal value i occurs chars[i] times
-        // the output should be comfortable for pipes
-        printf("%d\t%llu\n", i, chars[i]);
+        printf("%d\t%" PRIu64 "\n", i, bytes[i]);
     }
 }
 
