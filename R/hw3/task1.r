b99b6e8214c67db0bc88fb380c05cfd816133b98
Anton Petkov <anton.petkoff@gmail.com>
2017-01-21 10:53:04 +0200

    R: Seems like a correct solution to task 1 from hw3.

--- R/hw3/task1.r
+++ R/hw3/task1.r
@@ -4,9 +4,30 @@ library(UsingR)
 # H0: mu_m == mu_e
 # Ha: mu_m != mu_e
 blood
-qt(0.1 / 2, length(blood) - 1)
-qt(0.1 / 2, length(blood) - 1, lower.tail = FALSE)
-t.test(blood$Machine, blood$Expert, mu = 0, alt = "two.sided", sig.level = 0.1)
+alpha = 0.1
+df = length(blood) - 1
+qt(alpha / 2, df)
+qt(alpha / 2, df, lower.tail = FALSE)
+# rejection region is (-Inf, -6.313752) U (6.313752, Inf)
+
+m = blood$Machine
+e = blood$Expert
+
+# are the variances equal? no
+var(m)
+var(e)
+
+# normality of data - they seem normal to me...
+hist(m)
+hist(e)
+
+# test statistic
+(mean(m - e) * sqrt(length(m)))/sd(m - e)
+
+# p-value
+2 * pt(q = 0.681617, df = 14, lower.tail = FALSE)
+
+t.test(blood$Machine, blood$Expert, paired = TRUE)
 
 # task 2
 
