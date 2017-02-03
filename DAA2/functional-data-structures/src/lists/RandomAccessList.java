410b8de0181e03ae6637cd0a5808362537d765af
Anton Petkov <anton.petkoff@gmail.com>
2017-02-03 22:29:01 +0200

    DAA2: functional data structures: random access list: cons

---
+++ DAA2/functional-data-structures/src/lists/RandomAccessList.java
@@ -0,0 +1,76 @@
+package lists;
+
+import java.util.LinkedList;
+
+public class RandomAccessList {
+
+	public class Node {
+		int value;
+		Node left;
+		Node right;
+
+		public Node(int value) {
+			this.value = value;
+			this.left = null;
+			this.right = null;
+		}
+
+		@Override
+		public String toString() {
+			String left = this.left == null ? "null" : this.left.toString();
+			String right = this.right == null ? "null" : this.right.toString();
+			return "(" + left + ", " + this.value + ", " + right + ")";
+		}
+	}
+
+	public class Tree {
+		int size;
+		Node root;
+
+		public Tree(Node root, int size) {
+			this.root = root;
+			this.size = size;
+		}
+
+		@Override
+		public String toString() {
+			return "{size = " + this.size + ", root = " + this.root + "}";
+		}
+	}
+
+	LinkedList<Tree> forest;
+
+	public RandomAccessList() {
+		forest = new LinkedList<Tree>();
+	}
+
+	@Override
+	public String toString() {
+		return forest.toString();
+	}
+
+	public void cons(int value) {
+		if (forest.size() >= 2 && forest.get(0).size == forest.get(1).size) {
+			Tree tree1 = forest.removeFirst();
+			Tree tree2 = forest.removeFirst();
+
+			Node newRoot = new Node(value);
+			newRoot.left = tree1.root;
+			newRoot.right = tree2.root;
+
+			Tree combined = new Tree(newRoot, tree1.size + tree2.size + 1);
+			forest.addFirst(combined);
+		} else {
+			forest.addFirst(new Tree(new Node(value), 1));
+		}
+	}
+
+	public static void main(String[] args) {
+		RandomAccessList ral = new RandomAccessList();
+		ral.cons(1);
+		ral.cons(2);
+		ral.cons(3);
+		System.out.println(ral);
+	}
+
+}
