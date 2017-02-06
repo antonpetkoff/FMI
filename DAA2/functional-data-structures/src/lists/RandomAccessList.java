dc8fb1eb0c150d6efab842a73941b4866a50d218
Anton Petkov <anton.petkoff@gmail.com>
2017-02-06 18:54:02 +0200

    RandomAccessList: update list with path copying. replace tabs with spaces.

--- DAA2/functional-data-structures/src/lists/RandomAccessList.java
+++ DAA2/functional-data-structures/src/lists/RandomAccessList.java
@@ -2,136 +2,187 @@ package lists;
 
 public class RandomAccessList {
 
-	public static class Node {
-		int value;
-		Node left;
-		Node right;
+    public static class Node {
+        int value;
+        Node left;
+        Node right;
 
-		public Node(int value, Node left, Node right) {
-			this.value = value;
-			this.left = left;
-			this.right = right;
-		}
+        public Node(int value, Node left, Node right) {
+            this.value = value;
+            this.left = left;
+            this.right = right;
+        }
 
-		@Override
-		public String toString() {
-			String left = this.left == null ? "null" : this.left.toString();
-			String right = this.right == null ? "null" : this.right.toString();
-			return "(" + left + ", " + this.value + ", " + right + ")";
-		}
-	}
+        @Override
+        public String toString() {
+            String left = this.left == null ? "null" : this.left.toString();
+            String right = this.right == null ? "null" : this.right.toString();
+            return "(" + left + ", " + this.value + ", " + right + ")";
+        }
+    }
 
-	public static class Tree {
-		Node root;
-		int size;
-		Tree next;
+    public static class Tree {
+        Node root;
+        int size;
+        Tree next;
 
-		public Tree(Node root, int size, Tree next) {
-			this.root = root;
-			this.size = size;
-			this.next = next;
-		}
+        public Tree(Node root, int size, Tree next) {
+            this.root = root;
+            this.size = size;
+            this.next = next;
+        }
 
-		@Override
-		public String toString() {
-			return "{size = " + this.size + ", root = " + this.root + "}"
-				+ (this.next == null ? "" : (", " + this.next.toString()));
-		}
-	}
+        @Override
+        public String toString() {
+            return "{size = " + this.size + ", root = " + this.root + "}"
+                    + (this.next == null ? "" : (", " + this.next.toString()));
+        }
+    }
 
-	public static Tree empty() {
-		return null;
-	}
+    public static Tree empty() {
+        return null;
+    }
 
-	public static boolean isEmpty(Tree list) {
-		return list == empty();
-	}
+    public static boolean isEmpty(Tree list) {
+        return list == empty();
+    }
 
-	public static Tree cons(int value, Tree head) {
-		Tree tree1 = head;
-		Tree tree2 = tree1 != null ? tree1.next : null;
-		Tree result = null;
+    public static Tree cons(int value, Tree head) {
+        Tree tree1 = head;
+        Tree tree2 = tree1 != null ? tree1.next : null;
+        Tree result = null;
 
-		if (tree1 != null && tree2 != null && tree1.size == tree2.size) {
-			Node combinedRoot = new Node(value, tree1.root, tree2.root);
-			int combinedSize = tree1.size + tree2.size + 1;
-			result = new Tree(combinedRoot, combinedSize, tree2.next);
-		} else {
-			result = new Tree(new Node(value, null, null), 1, head);
-		}
+        if (tree1 != null && tree2 != null && tree1.size == tree2.size) {
+            Node combinedRoot = new Node(value, tree1.root, tree2.root);
+            int combinedSize = tree1.size + tree2.size + 1;
+            result = new Tree(combinedRoot, combinedSize, tree2.next);
+        } else {
+            result = new Tree(new Node(value, null, null), 1, head);
+        }
 
-		return result;
-	}
+        return result;
+    }
 
-	public static int head(Tree list) {
-		if (isEmpty(list)) {
-			throw new IllegalStateException();
-		}
+    public static int head(Tree list) {
+        if (isEmpty(list)) {
+            throw new IllegalStateException();
+        }
 
-		return list.root.value;
-	}
+        return list.root.value;
+    }
 
-	public static Tree tail(Tree list) {
-		if (isEmpty(list)) {
-			throw new IllegalStateException();
-		}
+    public static Tree tail(Tree list) {
+        if (isEmpty(list)) {
+            throw new IllegalStateException();
+        }
 
-		Tree result = null;
+        Tree result = null;
 
-		if (list.size == 1) {	// XXX: list.size is actually listHead.size
-			result = list.next;
-		} else {	// the size of the tree is 3, 7, 15, ..., 2^k - 1 for k > 1
-			int subTreeSize = list.size / 2;
-			Tree tree2 = new Tree(list.root.right, subTreeSize, list.next);
-			Tree tree1 = new Tree(list.root.left, subTreeSize, tree2);
-			result = tree1;
-		}
+        if (list.size == 1) {	// XXX: list.size is actually listHead.size
+            result = list.next;
+        } else {	// the size of the tree is 3, 7, 15, ..., 2^k - 1 for k > 1
+            int subTreeSize = list.size / 2;
+            Tree tree2 = new Tree(list.root.right, subTreeSize, list.next);
+            Tree tree1 = new Tree(list.root.left, subTreeSize, tree2);
+            result = tree1;
+        }
 
-		return result;
-	}
+        return result;
+    }
 
-	public static int get(Tree list, int index) {
-		if (isEmpty(list)) {
-			throw new IndexOutOfBoundsException();
-		}
+    public static int get(Tree list, int index) {
+        if (isEmpty(list)) {
+            throw new IndexOutOfBoundsException();
+        }
 
-		return forestLookUp(list, index);
-	}
+        return forestLookUp(list, index);
+    }
 
-	private static int forestLookUp(Tree head, int index) {
-		Tree tree = head;
+    private static int forestLookUp(Tree head, int index) {
+        Tree tree = head;
 
-		while (index >= tree.size) {
-			index -= tree.size;
-			tree = tree.next;
-		}
+        while (index >= tree.size) {
+            index -= tree.size;
+            tree = tree.next;
+        }
 
-		return treeLookUp(tree.root, tree.size, index);
-	}
+        return treeLookUp(tree.root, tree.size, index);
+    }
 
-	private static int treeLookUp(Node root, int size, int index) {
-		if (index < 0 || index >= size) {
-			throw new IndexOutOfBoundsException();
-		}
+    private static int treeLookUp(Node root, int size, int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
 
-		if (index == 0) {
-			return root.value;
-		} else {
-			int subTreeSize = size / 2;
+        if (index == 0) {
+            return root.value;
+        } else {
+            int subTreeSize = size / 2;
 
-			if (index <= subTreeSize) {
-				return treeLookUp(root.left, subTreeSize, index - 1);
-			} else {
-				return treeLookUp(root.right, subTreeSize, index - 1 - subTreeSize);
-			}
-		}
-	}
+            if (index <= subTreeSize) {
+                return treeLookUp(root.left, subTreeSize, index - 1);
+            } else {
+                return treeLookUp(root.right, subTreeSize, index - 1 - subTreeSize);
+            }
+        }
+    }
 
-	public static void main(String[] args) {
-		Tree list = cons(3, cons(2, cons(1, empty())));
-		System.out.println(list);
-		System.out.println(get(list, 0));
-		System.out.println(tail(list));
-	}
+    public static Tree update(Tree list, int index, int value) {
+        if (isEmpty(list)) {
+            throw new IllegalStateException();
+        }
+
+        return updateForest(list, index, value);
+    }
+
+    // forest path copying
+    private static Tree updateForest(Tree listHead, int index, int value) {
+        // create a clone and make all modifications on the clone
+        Tree updatedList = new Tree(listHead.root, listHead.size, listHead.next);  // TODO: listHead.clone()
+
+        Tree head = updatedList;    // start forest path copying
+
+        while (index >= head.size) {
+            index -= head.size;
+            head = head.next = new Tree(head.next.root, head.next.size, head.next.next);    // TODO: head.next.clone()
+        }
+
+        // finish forest path copying by starting in-tree path copying
+        head.root = updateTree(head.root, head.size, index, value);
+
+        return updatedList;
+    }
+
+    // tree path copying
+    private static Node updateTree(Node root, int size, int index, int value) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        if (index == 0) {   // base
+            return new Node(value, root.left, root.right);
+        }
+
+        // step, clone node and update one branch
+        int subTreeSize = size / 2;
+
+        if (index <= subTreeSize) {
+            Node updatedLeftSubtree = updateTree(root.left, subTreeSize, index - 1, value);
+            return new Node(root.value, updatedLeftSubtree, root.right);
+        } else {
+            Node updatedRightSubtree = updateTree(root.right, subTreeSize, index - 1 - subTreeSize, value);
+            return new Node(root.value, root.left, updatedRightSubtree);
+        }
+    }
+
+    public static void main(String[] args) {
+        Tree list = cons(3, cons(2, cons(1, empty())));
+        System.out.println(list);
+        System.out.println(get(list, 0));
+        System.out.println(tail(list));
+
+        Tree updatedList = update(cons(4, list), 3, 42);
+        System.out.println(updatedList);
+    }
 
 }
